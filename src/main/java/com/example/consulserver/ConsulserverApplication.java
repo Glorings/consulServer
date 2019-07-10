a920edd9f7617fd323f056a486303949e55210cf
Glorings <18668465735@163.com>
2019-07-10 15:02:49 +0800

    init

---
+++ src/main/java/com/example/consulserver/ConsulserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.consulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulserverApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/consulserver/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.example.consulserver.controller;
+
+
+
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TestController {
+    protected final org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * 注意：新版Spring Cloud Consul 默认注册健康检查接口为：/actuator/health
+     * @return SUCCESS
+     */
+    @GetMapping("/actuator/health")
+    public String health() {
+        return "SUCCESS";
+    }
+
+    /**
+     * 提供 sayHello 服务:根据对方传来的名字 XX，返回:hello XX
+     * @return String
+     */
+    @GetMapping("/sayHello")
+    public String sayHello(String name){
+        return "hello," + name;
+    }
+}
